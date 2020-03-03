4e4015bfa9a0e34bb177b15679313654a18f8db6
Lym-mg <1292043630@qq.com>
2020-03-03 12:55:13 +0800

    新增 @interface MGImage : NSObject

---
+++ MGTest/MGTest/MGImage.m
@@ -0,0 +1,13 @@
+//
+//  MGImage.m
+//  MGTest
+//
+//  Created by newunion on 2020/3/3.
+//  Copyright © 2020 newunion. All rights reserved.
+//
+
+#import "MGImage.h"
+
+@implementation MGImage
+
+@end
