42841557893d024302aa0c7089fa49ce8fbbff0c
Lym-mg <1292043630@qq.com>
2020-03-03 13:06:02 +0800

    新增XJILView

---
+++ MGTest/MGTest/XJLView.h
@@ -0,0 +1,17 @@
+//
+//  XJLView.h
+//  MGTest
+//
+//  Created by newunion on 2020/3/3.
+//  Copyright © 2020 newunion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XJLView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
